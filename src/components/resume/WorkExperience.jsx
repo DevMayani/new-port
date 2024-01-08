6f2e6fcf9ad334fa0487f61bea16a1abc2372bc8
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-01-08 09:03:37 +0100

    add new

--- src/components/resume/WorkExperience.jsx
+++ src/components/resume/WorkExperience.jsx
@@ -6,7 +6,7 @@ const WorkExperience = [
         company: "Nencia",
         yearsActive: "(Dec 17  2022, - Feb 10  2023)",
         information: [
-            "As a frontend developer,the major duty was to carry out varieties of project and team work helped in the efficiency and accuracy of meeting up with deadlines.",
+            "As a frontend developer intern,the major duty was to carry out varieties of project and team work helped in the efficiency and accuracy of meeting up with deadlines.",
             "Used React js and Tailwind Css to build an Admin dash board,a nencia wedding app and other projects.",
            
         ],
@@ -17,7 +17,7 @@ const WorkExperience = [
         company: "Austech-Ic",
         yearsActive: "(April 10 2022, - Present)",
         information: [
-            "As a frontend developer intern,i was promoted to the role of a frontend developer, i built a Property Management System using Next.js, A Content Management System using React.js, Built a landing page and other pages for the web version of a Integrated Health and Wellness Platform that caters to both physical and mental health, using Next js.",
+            "As a frontend developer, i was promoted to the role of a frontend developer, i built a Property Management System using Next.js, A Content Management System using React.js, Built a landing page and other pages for the web version of a Integrated Health and Wellness Platform that caters to both physical and mental health, using Next js.",
             "Worked to continually meet the expectation of our clients.",
         ],
     },
