c6a7e672802996b198b8d73116b46504c7a32f91
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-09-09 14:12:04 +0100

    add new commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,10 @@
-
 import { useForm, ValidationError } from '@formspree/react';
-import $ from 'jquery'; 
-
 import "./Contact.css";
 import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
     const [state, handleSubmit] = useForm("mgebyynj");
    
-    if (state.succeeded) {
-        return <p>Successfully sent email.</p>;
-    };
-
-    $('form').submit(function () {
-
-        var text = $.trim($('#text').val());
-        var message = $.trim($('#message').val());
-        var email =$.trim($('#email').val());
-    
-        if (text === '') {
-            alert('Input a text.');
-            return false;
-        }
-        else if (message === '') {
-            alert('Input a message.');
-            return false;
-        }
-        else if (email === '') {
-            alert('Input email address.');
-            return false;
-        }
-    });
-    
     return (
         <section className="contact container section" id="contact">
             <h2 className="section__title">Get In Touch</h2>
@@ -44,76 +17,79 @@ const Contact = () => {
                     </p>
                 </div>
 
-                <form  onSubmit={handleSubmit} className="contact__form">
-                    <div className="contact__form-group">
-                        <div className="contact__form-div">
-                            <input
-                                className="contact__form-input"
-                                placeholder="Insert your name"
-                                id="text"
-                                required type="text" 
-                                name="text"
-                               
-                            />
-                            <ValidationError 
-                               prefix="Text" 
-                               field="text"
-                               errors={state.errors}
-                            />
+                {state.succeeded ? (
+                   <div className='success'>  <p>Successfully sent email.</p></div>
+                ) : (
+                    <form onSubmit={handleSubmit} className="contact__form">
+                        <div className="contact__form-group">
+                            <div className="contact__form-div">
+                                <input
+                                    className="contact__form-input"
+                                    placeholder="Insert your name"
+                                    id="text"
+                                    required type="text" 
+                                    name="text"
+                                />
+                                <ValidationError 
+                                    prefix="Text" 
+                                    field="text"
+                                    errors={state.errors}
+                                />
+                            </div>
+
+                            <div className="contact__form-div">
+                                <input
+                                    className="contact__form-input"
+                                    placeholder="Insert your email"
+                                    id="email"
+                                    required type="email" 
+                                    name="email"
+                                />
+                                <ValidationError 
+                                    prefix="Email" 
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </div>
                         </div>
 
                         <div className="contact__form-div">
                             <input
-                                className="contact__form-input"
-                                placeholder="Insert your email"
-                                id="email"
-                                required type="email" 
-                                name="email"
-                            />
-                            <ValidationError 
-                               prefix="Email" 
-                               field="email"
-                               errors={state.errors}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="contact__form-div">
-                        <input
                                 className="contact__form-input"
                                 placeholder="Insert your subject"
-                                id="text"
+                                id="subject" 
                                 required type="text"  
-                                name="text"
+                                name="subject"
                             />
                             <ValidationError 
-                               prefix="Text" 
-                               field="text"
-                               errors={state.errors}
+                                prefix="Subject" 
+                                field="subject"
+                                errors={state.errors}
                             />
-                        
-                    </div>
+                        </div>
 
-                    <div className="contact__form-div contact__form-area">
-                        <textarea
-                            id="message"
-                            name="message"
-                            cols="30"
-                            rows="10"
-                            required type="text" 
-                            className="contact__form-input"
-                            placeholder="Write your message" />
+                        <div className="contact__form-div contact__form-area">
+                            <textarea
+                                id="message"
+                                name="message"
+                                cols="30"
+                                rows="10"
+                                required type="text" 
+                                className="contact__form-input"
+                                placeholder="Write your message" 
+                            />
                             <ValidationError 
-                              prefix="Message" 
-                            field="message"
-                            errors={state.errors}
+                                prefix="Message" 
+                                field="message"
+                                errors={state.errors}
                             />
-                    </div>
+                        </div>
 
-                    <button type="submit" disabled={state.submitting} className="btn">
-                        Send Message
-                    </button>
-                </form>
+                        <button type="submit" disabled={state.submitting || state.succeeded} className="btn">
+                            Send Message
+                        </button>
+                    </form>
+                )}
                 
             </div>
         </section>
